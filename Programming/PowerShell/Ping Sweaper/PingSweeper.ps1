13f354af542c068bbf2b1f25898c9f43b4b89b81
Ryan Young <youngrya001@gmail.com>
2023-07-18 14:24:28 -0400

    Create PingSweeper.ps1

---
+++ Programming/PowerShell/Ping Sweaper/PingSweeper.ps1
@@ -0,0 +1,40 @@
+$ipAddresses = Get-Content -Path "$env:USERPROFILE\Documents\ping.txt"
+$outputFile = "$env:USERPROFILE\Documents\ping_results.txt"
+
+foreach ($ip in $ipAddresses) {
+    if ($ip -like '*\*') {
+        $rangeStart, $rangeEnd = $ip -split '\s+'
+        $rangeStart = $rangeStart.Trim()
+        $rangeEnd = $rangeEnd.Trim()
+        
+        $rangeStartIP = [System.Net.IPAddress]::Parse($rangeStart)
+        $rangeEndIP = [System.Net.IPAddress]::Parse($rangeEnd)
+        
+        $currentIP = $rangeStartIP
+        
+        while ($currentIP.CompareTo($rangeEndIP) -le 0) {
+            $pingReply = Test-Connection -ComputerName $currentIP.IPAddressToString -Count 1 -Quiet
+            
+            if ($pingReply) {
+                "$($currentIP.IPAddressToString): Reply" | Out-File -Append -FilePath $outputFile
+            }
+            else {
+                "$($currentIP.IPAddressToString): No Reply" | Out-File -Append -FilePath $outputFile
+            }
+            
+            $currentIP = [System.Net.IPAddress]::Parse($currentIP.GetAddressBytes() + 1)
+        }
+    }
+    else {
+        $pingReply = Test-Connection -ComputerName $ip -Count 1 -Quiet
+    
+        if ($pingReply) {
+            "$ip: Reply" | Out-File -Append -FilePath $outputFile
+        }
+        else {
+            "$ip: No Reply" | Out-File -Append -FilePath $outputFile
+        }
+    }
+}
+
+Write-Host "Ping sweep completed. Results saved in $outputFile"
