aa35f29c8fb10f325664f9a1d6e261ada2d050a5
Ryan Young <youngrya001@gmail.com>
2023-07-26 14:02:22 -0400

    Create count_commits.py

---
+++ Programming/Python 3/count_commits.py
@@ -0,0 +1,11 @@
+import subprocess
+
+folder_path = "Tutorials/Programming/Python 3"
+
+# Get commit count for the specific folder
+cmd = f'git rev-list --count HEAD -- "{folder_path}"'
+commit_count = subprocess.check_output(cmd, shell=True).decode().strip()
+
+# Write the commit count to a file
+with open('commit_count.txt', 'w') as f:
+    f.write(commit_count)
